b086f3dd87a07baf1ff7d2bb4275f3634c176b24
David Fonseca <fonsecasoundcruz@gmail.com>
2022-04-19 11:10:45 -0500

    Add repository to GitHub

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdministracionComponent } from './administracion/administracion.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RolesComponent } from './roles/roles.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent },
+  {path: 'usuarios', component :  UsuariosComponent},
+  {path : 'roles', component : RolesComponent},
+  {path: 'administracion', component : AdministracionComponent},
+  {path : 'inventario', component: InventarioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import {MaterialModule} from './material/material.module';
+
+import {MenuComponent} from './menu/menu.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { RolesComponent } from './roles/roles.component';
+import { AdministracionComponent } from './administracion/administracion.component';
+import { InventarioComponent } from './inventario/inventario.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LandingPageComponent,
+    UsuariosComponent,
+    RolesComponent,
+    AdministracionComponent,
+    InventarioComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
